f87ff42f5954eb3c7794e3f47f89d75b17b30252
Tom McLaughlin <tmclaugh@sdf.lonestar.org>
2012-06-27 22:05:49 -0400

    Simplify this module.  No need to handle 'present' or 'absent'.  No need for OS specific configuration.  Plus, I just append an include line for sudoers.d into sudoers.

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,34 +2,6 @@
 #
 # This module manages sudo
 #
-# Parameters:
-#   [*ensure*]
-#     Ensure if present or absent.
-#     Default: present
-#
-#   [*autoupgrade*]
-#     Upgrade package automatically, if there is a newer version.
-#     Default: false
-#
-#   [*package*]
-#     Name of the package.
-#     Only set this, if your platform is not supported or you know, what you're doing.
-#     Default: auto-set, platform specific
-#
-#   [*config_file*]
-#     Main configuration file.
-#     Only set this, if your platform is not supported or you know, what you're doing.
-#     Default: auto-set, platform specific
-#
-#   [*config_file_replace*]
-#     Replace configuration file with that one delivered with this module
-#     Default: true
-#
-#   [*config_dir*]
-#     Main configuration directory
-#     Only set this, if your platform is not supported or you know, what you're doing.
-#     Default: auto-set, platform specific
-#
 # Actions:
 #   Installs locales package and generates specified locales
 #
@@ -37,59 +9,37 @@
 #   Nothing
 #
 # Sample Usage:
-#   class { 'locales':
-#     locales => [ 'en_US.UTF-8 UTF-8', 'de_DE.UTF-8 UTF-8', 'en_GB.UTF-8 UTF-8', ],
+#   node foo { 
+#		include sudo	
 #   }
 #
 # [Remember: No empty lines between comments and class definition]
-class sudo(
-  $ensure = 'present',
-  $autoupgrade = false,
-  $package = $sudo::params::package,
-  $config_file = $sudo::params::config_file,
-  $config_file_replace = true,
-  $config_dir = $sudo::params::config_dir,
+class sudo inherits sudo::params {
 
-) inherits sudo::params {
-
-  case $ensure {
-    /(present)/: {
-      $dir_ensure = 'directory'
-      if $autoupgrade == true {
-        $package_ensure = 'latest'
-      } else {
-        $package_ensure = 'present'
-      }
-    }
-    /(absent)/: {
-      $package_ensure = 'absent'
-      $dir_ensure = 'absent'
-    }
-    default: {
-      fail('ensure parameter must be present or absent')
-    }
+  package { 'sudo':
+    ensure => 'present',
   }
 
-  package { $package:
-    ensure => $package_ensure,
-  }
-
-  file { $config_file:
-    ensure  => $ensure,
+  file { "$config_file":
     owner   => 'root',
     group   => 'root',
     mode    => '0440',
-    replace => $config_file_replace,
-    require => Package[$package],
+    require => Package['sudo'],
   }
   
-  file { $config_dir:
-    ensure  => $dir_ensure,
+  file { "$config_dir":
+    ensure  => directory,
+    recurse => true,
     owner   => 'root',
     group   => 'root',
     mode    => '0550',
-    recurse => true,
-    purge   => true,
-    require => Package[$package],
+    require => Package['sudo'],
   }
+  
+  append_if_no_match { "inc_sudoers_dir" :
+		file => $config_file,
+		line => "\n#includedir $config_dir",
+		match => "#includedir.*${config_dir}",
+		require => [ File[$config_file], File["$config_dir"] ],
+	}
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,17 +1,4 @@
 class sudo::params {
-  case $::operatingsystem {
-    ubuntu, debian, gentoo: {
-      $package = 'sudo'
-      $config_file = '/etc/sudoers'
-      $config_dir = '/etc/sudoers.d/'
-    }
-    redhat, centos, fedora: {
-      $package = 'sudo'
-      $config_file = '/etc/sudoers'
-      $config_dir = '/etc/sudoers.d/'
-    }
-    default: {
-      fail("Unsupported platform: ${::operatingsystem}")
-    }
-  }
+  $config_file = '/etc/sudoers'
+  $config_dir = '/etc/sudoers.d/'
 }
